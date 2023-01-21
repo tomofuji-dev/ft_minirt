f13079f8b18d4f86640354d0df45eaa3d0d9bc55
chanma <yunosuke.homma@icloud.com>
2023-01-21 19:05:15 +0900

    modify

--- src/draw/raytrace.c
+++ src/draw/raytrace.c
@@ -15,23 +15,21 @@
 #include "draw.h"
 #include "color.h"
 #include <math.h>
-#include <stdio.h>
+
+static void	set_info(t_info *info, double max_dist, \
+					bool exit_once_found, bool out_needed);
 
 bool	raytrace(const t_scene *scene, const t_ray *eye_ray, t_rgb *rgb)
 {
 	t_shape_intp	shape_intp;
-	size_t		i;
-	t_raytrace	vars;
-	t_info		info;
+	size_t			i;
+	t_raytrace		vars;
+	t_info			info;
 
-	info.max_dist = INFINITY;
-	info.exit_once_found = false;
-	info.out_needed = true;
+	set_info(&info, INFINITY, false, true);
 	if (get_nearest_shape(scene, eye_ray, info, &shape_intp))
 	{
-		rgb->r = scene->ambient_illuminance.r * shape_intp.shape->material.ambient_ref.r;
-		rgb->g = scene->ambient_illuminance.g * shape_intp.shape->material.ambient_ref.g;
-		rgb->b = scene->ambient_illuminance.b * shape_intp.shape->material.ambient_ref.b;
+		multiple_rgb(rgb, scene->ambient_illuminance, shape_intp.shape->material.ambient_ref);
 		i = 0;
 		while (i < scene->num_lights)
 		{
@@ -50,16 +48,14 @@ bool	raytrace(const t_scene *scene, const t_ray *eye_ray, t_rgb *rgb)
 			}
 			vars.shadow_ray.start = add_vec(shape_intp.intp.position, constant_mul_vec(vars.l, C_EPSILON));
 			vars.shadow_ray.direction = vars.l;
-			info.max_dist = vars.dl;
-			info.exit_once_found = true;
-			info.out_needed = true;
+			set_info(&info, vars.dl, true, false);
 			if (get_nearest_shape(scene, &vars.shadow_ray, info, &shape_intp))
 			{
 				i++;
 				continue ;
 			}
 			vars.nl_dot = inner_product(shape_intp.intp.normal, vars.l);
-			vars.nl_dot = clamp(vars.nl_dot, 0, 1);
+			vars.nl_dot = ft_clamp(vars.nl_dot, 0, 1);
 			add_on_rgb(rgb, shape_intp.shape->material.diffuse_ref, scene->lights[i], vars.nl_dot);
 			if (vars.nl_dot > 0)
 			{
@@ -68,16 +64,21 @@ bool	raytrace(const t_scene *scene, const t_ray *eye_ray, t_rgb *rgb)
 				vars.v = norm_vec(constant_mul_vec(\
 										eye_ray->direction, -1));
 				vars.vr_dot = inner_product(vars.v, vars.r);
-				vars.vr_dot = clamp(vars.vr_dot, 0, 1);
+				vars.vr_dot = ft_clamp(vars.vr_dot, 0, 1);
 				vars.vr_dot_pow = pow(vars.vr_dot, shape_intp.shape->material.shininess);
 				add_on_rgb(rgb, shape_intp.shape->material.specular_ref, scene->lights[i], vars.vr_dot_pow);
 			}
 			i++;
 		}
-		rgb->r = 255 * clamp(rgb->r, 0, 1);
-		rgb->g = 255 * clamp(rgb->g, 0, 1);
-		rgb->b = 255 * clamp(rgb->b, 0, 1);
+		clamp_mul_rgb(rgb, 255);
 		return (true);
 	}
 	return (false);
+}
+
+static void	set_info(t_info *info, double max_dist, bool exit_once_found, bool output_needed)
+{
+	info->max_dist = max_dist;
+	info->exit_once_found = exit_once_found;
+	info->output_needed = output_needed;
 }
