db0da38225851bad897deaf6d69dd28d70dffbb5
Tomoya Fujiwara <tomofuji.dev@gmail.com>
2023-01-22 11:15:49 +0900

    modify init_shape for norminette

--- src/init/init_shape.c
+++ src/init/init_shape.c
@@ -6,7 +6,7 @@
 /*   By: tfujiwar <tfujiwar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 16:01:53 by tfujiwar          #+#    #+#             */
-/*   Updated: 2023/01/22 10:32:09 by tfujiwar         ###   ########.fr       */
+/*   Updated: 2023/01/22 11:14:46 by tfujiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,20 +28,18 @@ bool	init_sphere(t_scene *scene, char ***splited)
 	t_sphere		*sph;
 	t_rgb			rgb;
 
-	if (!is_valid_format(splited, tp_len, dp_lens))
-		return (false);
-	if (!append_shape(scene))
+	if (!is_valid_format(splited, tp_len, dp_lens) \
+		|| !append_shape(scene))
 		return (false);
 	shape = lst_last_shape(scene->shape);
 	shape->type = ST_SPHERE;
 	sph = &shape->u_data.sphere;
-	if (!is_valid_vec(splited[1], &sph->center, false))
+	if (!is_valid_vec(splited[1], &sph->center, false) \
+		|| !rt_strtod(splited[2][0], &sph->radius))
 		return (false);
-	if (!rt_strtod(splited[2][0], &sph->radius))
-		return (false);
-	sph->radius /= 2;
 	if (!is_valid_rgb(splited[3], &rgb))
 		return (false);
+	sph->radius /= 2;
 	shape->material.diffuse_ref = calc_rgb_ratio(rgb, 1.0);
 	default_material(&shape->material);
 	return (true);
@@ -55,16 +53,14 @@ bool	init_plane(t_scene *scene, char ***splited)
 	t_plane			*pl;
 	t_rgb			rgb;
 
-	if (!is_valid_format(splited, tp_len, dp_lens))
-		return (false);
-	if (!append_shape(scene))
+	if (!is_valid_format(splited, tp_len, dp_lens) \
+		|| !append_shape(scene))
 		return (false);
 	shape = lst_last_shape(scene->shape);
 	shape->type = ST_PLANE;
 	pl = &shape->u_data.plane;
-	if (!is_valid_vec(splited[1], &pl->position, false))
-		return (false);
-	if (!is_valid_vec(splited[2], &pl->normal, true))
+	if (!is_valid_vec(splited[1], &pl->position, false) \
+		|| !is_valid_vec(splited[2], &pl->normal, true))
 		return (false);
 	if (!is_valid_rgb(splited[3], &rgb))
 		return (false);
@@ -81,24 +77,20 @@ bool	init_cylinder(t_scene *scene, char ***splited)
 	t_cylinder		*cy;
 	t_rgb			rgb;
 
-	if (!is_valid_format(splited, tp_len, dp_lens))
-		return (false);
-	if (!append_shape(scene))
+	if (!is_valid_format(splited, tp_len, dp_lens) \
+		|| !append_shape(scene))
 		return (false);
 	shape = lst_last_shape(scene->shape);
 	shape->type = ST_CYLINDER;
 	cy = &shape->u_data.cylinder;
-	if (!is_valid_vec(splited[1], &cy->position, false))
-		return (false);
-	if (!is_valid_vec(splited[2], &cy->direction, true))
-		return (false);
-	if (!rt_strtod(splited[3][0], &cy->radius))
-		return (false);
-	cy->radius /= 2;
-	if (!rt_strtod(splited[4][0], &cy->height))
+	if (!is_valid_vec(splited[1], &cy->position, false) \
+		|| !is_valid_vec(splited[2], &cy->direction, true) \
+		|| !rt_strtod(splited[3][0], &cy->radius) \
+		|| !rt_strtod(splited[4][0], &cy->height))
 		return (false);
 	if (!is_valid_rgb(splited[5], &rgb))
 		return (false);
+	cy->radius /= 2;
 	shape->material.diffuse_ref = calc_rgb_ratio(rgb, 1.0);
 	default_material(&shape->material);
 	return (true);
