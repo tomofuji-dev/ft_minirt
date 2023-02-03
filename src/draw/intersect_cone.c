caffbe3d35561f964cc915b8e2eeafe532ffeeec
Tomoya Fujiwara <tomofuji.dev@gmail.com>
2023-02-03 12:53:17 +0900

    add height condition to cone

--- src/draw/intersect_cone.c
+++ src/draw/intersect_cone.c
@@ -6,7 +6,7 @@
 /*   By: tfujiwar <tfujiwar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 13:12:58 by hommayunosu       #+#    #+#             */
-/*   Updated: 2023/01/27 11:45:34 by tfujiwar         ###   ########.fr       */
+/*   Updated: 2023/02/03 12:51:16 by tfujiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,10 +53,14 @@ static t_discrim	intersect_cone_discrim(const t_shape *shape, \
 {
 	const t_cone	*cone;
 	t_discrim		d;
+	double			distance_to_intersect;
 
 	cone = &shape->u_data.cone;
 	set_discrim_structure(&d, cone, ray);
 	process_discrim(&d);
+	distance_to_intersect = abs_vec(diff_vec(add_vec(ray->start, constant_mul_vec(ray->direction, d.t)), cone->position));
+	if (distance_to_intersect < -cone->height || distance_to_intersect > cone->height)
+		d.t = -1;
 	return (d);
 }
 
